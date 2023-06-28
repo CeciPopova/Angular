2ddfaf6eca83217f2d2dfa1ae4c847371bcaf899
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-06-28 17:45:03 +0300

    install typescript global

---
+++ Intro to Angular and TypeScript/playground.ts
@@ -0,0 +1,16 @@
+type User1 = {
+    name: string;
+    age: number;
+}
+
+
+interface User2  {
+    name: string;
+    age: number;
+}
+
+class UserTest implements User2 {
+    name: string;
+    age: number;
+
+}
