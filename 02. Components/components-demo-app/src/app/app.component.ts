dab01a6c4225796958559ce48bcd49cd7817c639
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-04 22:31:22 +0300

    Create home and navigation new components

--- 02. Components/components-demo-app/src/app/app.component.ts
+++ 02. Components/components-demo-app/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'],
+  styles: [
+   `
+   div {
+    background: pink;
+   }
+   `,
+  ]
 })
 export class AppComponent {
-  title = 'components-demo-app';
+  title = 'title from appComponent';
 }
