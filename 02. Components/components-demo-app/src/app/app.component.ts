0de6c1f46b8e11d92d42e62c28dd015efe05a422
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-06 14:32:24 +0300

    playing with components

--- 02. Components/components-demo-app/src/app/app.component.ts
+++ 02. Components/components-demo-app/src/app/app.component.ts
@@ -3,15 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles: [
-   `
-   div {
-    background: pink;
-   }
-   `,
-  ]
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //  `
+   /* div { */
+    /* background: pink; */
+   /* } */
+   /* `, */
+  // ]
 })
 export class AppComponent {
   title = 'title from appComponent';
+
+  users = [
+    { name: 'Mitko', age: 21 },
+    { name: 'Pesho', age: 22 },
+    { name: 'Mariika', age: 13 },
+    { name: 'Katia', age: 19 },
+    
+  ];
+
+  onTestOutput(event: Event) {
+    console.log('onTestOutput');
+    console.log(event);
+  }
 }
--- 02. Components/components-demo-app/src/app/navigation/navigation.component.ts
+++ 02. Components/components-demo-app/src/app/navigation/navigation.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnChanges {
+  @Input('title') titleFromApp: string = '';
+  @Input('activeUsers') activeUsers: { name: string; age: number }[] = [];
+  @Output() onTestOutput = new EventEmitter<boolean>();
   
   isActive = false;
 
+  inputValue = 'Hello!';
 
-  handleClick(): void {
+  ngOnInit(): void {
+    console.log('navigation-> init');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+
+
+  handleClick(event: Event): void {
     this.isActive = !this.isActive;
-    console.log('Clicked!');
+    this.onTestOutput.emit(this.isActive);
   }
 }
