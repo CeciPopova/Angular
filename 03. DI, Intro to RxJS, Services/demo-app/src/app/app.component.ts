d5f61ae1e9c2e848222288c7370a84008123c3ba
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-08 10:03:14 +0300

    works with change detection!

--- 03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ 03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -15,14 +15,31 @@ export class AppComponent {
     {name: 'Pesho', age: 16}, 
     {name: 'Penka', age: 56},
   ];
+  /**
+   *
+   */
+  constructor() {
+   setInterval(() => {
+    this.users.push({
+      name: 'DemoName',
+      age: 0,
+    });
+
+    console.log('User has been added!');
+   }, 3000);
+    
+  }
 
   addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
     const user = {
       name: inputName.value,
       age: Number(inputAge.value)
     };
+//On this way works on Defaulth ChangeDetectorStrategy!
+    //this.users.push(user);
 
-    this.users.push(user);
+//On this way works on OnPush ChangeDetectorStrategy!
+    this.users = [...this.users, user];
 
     inputName.value = '';
     inputAge.value = '';
