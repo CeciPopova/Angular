fef7ea7ac1eb5fd01a141fdfe8d44bb30889ce70
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-08 09:49:02 +0300

    Create components and add users

--- 03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ 03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './types/User';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-app';
+
+  users: User[] = [
+    {name: 'Ivan', age: 21},
+    {name: 'Mitko', age: 28}, 
+    {name: 'Pesho', age: 16}, 
+    {name: 'Penka', age: 56},
+  ];
+
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    const user = {
+      name: inputName.value,
+      age: Number(inputAge.value)
+    };
+
+    this.users.push(user);
+
+    inputName.value = '';
+    inputAge.value = '';
+  }
 }
