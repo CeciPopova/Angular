f896f6feefda5f663ba1ef0fcd18599488ad043a
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-27 13:16:21 +0300

    Take dynamics data from Rest-api

--- workshop/components-workshop/src/app/themes-list/themes-list.component.ts
+++ workshop/components-workshop/src/app/themes-list/themes-list.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
-  styleUrls: ['./themes-list.component.css']
+  styleUrls: ['./themes-list.component.css'],
 })
-export class ThemesListComponent implements OnInit{
+export class ThemesListComponent implements OnInit {
+  themesList: Theme[] = [];
   constructor(private apiService: ApiService) {}
-  
+
   ngOnInit(): void {
-    this.apiService.getThemes().subscribe((themes) => 
-    console.log(themes)
-    )}
+    this.apiService
+      .getThemes()
+      .subscribe((themes) =>{
+        this.themesList = themes;
+        //console.log(themes[0]);
+      })
+  }
 }
