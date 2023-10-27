9d66e80eb0ab891b35e54b9b798ac2a1245ba6e6
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-27 11:26:00 +0300

    Create interface Theme and Post

---
+++ workshop/components-workshop/src/app/types/theme.ts
@@ -0,0 +1,12 @@
+import { UserId } from "./user-id";
+
+export interface Theme {
+  subscribers: string[];
+  posts: string[];
+  _id: '5fa64a9f2183ce1728ff371a';
+  themeName: string;
+  userId: UserId;
+  created_at: string;
+  updatedAt: string;
+  __v: number;
+}
