a668b49f7032276a9e1aa26147a59a375e1b0b46
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-30 12:52:13 +0200

    update user.service.ts

--- 04. Modules and Routing/modules-and-routing/src/app/user/user.service.ts
+++ 04. Modules and Routing/modules-and-routing/src/app/user/user.service.ts
@@ -1,16 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { User } from '../types/User';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService implements OnInit {
+export class UserService {
 
   constructor(private http: HttpClient) { }
-  ngOnInit(): void {
-    //throw new Error('Method not implemented.');
-  }
 
   fetchUsers() {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
