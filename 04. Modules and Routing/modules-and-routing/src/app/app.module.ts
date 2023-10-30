29e60dc76a197f4c479b282be29ed468bffa2183
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-10-30 12:33:01 +0200

    generate module user with component user-list, then generate service user and interface User. implement function OnInit  in user.service.ts  and create the function fetchUsers()

--- 04. Modules and Routing/modules-and-routing/src/app/app.module.ts
+++ 04. Modules and Routing/modules-and-routing/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+    UserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
