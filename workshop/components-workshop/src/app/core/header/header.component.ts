7425e39b65e9923d82f9c2d8fdcba96658bd8080
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-13 12:31:03 +0200

    import RouterModule in core.module.ts

--- workshop/components-workshop/src/app/core/header/header.component.ts
+++ workshop/components-workshop/src/app/core/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isLoggedIn: boolean = false;
+  isLoggedIn: boolean = true;
 
 
 }
