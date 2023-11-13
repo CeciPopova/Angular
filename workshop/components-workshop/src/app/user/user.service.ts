5baacc928079d4e2a359b6b07b14aac7e9dc4052
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-13 13:20:45 +0200

    add interface User

--- workshop/components-workshop/src/app/user/user.service.ts
+++ workshop/components-workshop/src/app/user/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import { User } from '../types/user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: User | undefined;
 
   constructor() { }
 }
