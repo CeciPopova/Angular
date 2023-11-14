93876b11d832c0b476a2d24552f7b910b107d172
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-14 21:46:21 +0200

    create guard AuthActivate implements CanActivate and insert in in Routes in user-routing.module.ts

--- workshop/components-workshop/src/app/user/user-routing.module.ts
+++ workshop/components-workshop/src/app/user/user-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../core/guards/auth.activate';
 
 
 const routes: Routes = [
   
   {
-    path: 'login', component: LoginComponent
+    path: 'login', component: LoginComponent, canActivate: [AuthActivate]
   },
 
   {
