dee46acc750586a2710e4c5cea4cbb8590e51c95
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-15 22:19:51 +0200

    using userService to show info for logged in user

--- workshop/components-workshop/src/app/theme/current-theme/current-theme.component.ts
+++ workshop/components-workshop/src/app/theme/current-theme/current-theme.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { Theme } from 'src/app/types/theme';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-current-theme',
@@ -11,18 +12,26 @@ import { Theme } from 'src/app/types/theme';
 export class CurrentThemeComponent implements OnInit {
   theme: Theme | undefined;
 
-  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
   ngOnInit(): void {
     this.fetchTheme();
   }
 
-  fetchTheme():void {
+  fetchTheme(): void {
     const id = this.activatedRoute.snapshot.params['themeId'];
 
-    this.apiService.getTheme(id).subscribe(theme => {
+    this.apiService.getTheme(id).subscribe((theme) => {
       this.theme = theme;
       console.log({ theme });
     });
-
   }
 }
