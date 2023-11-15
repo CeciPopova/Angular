5c31f7e17fe6dec66dfe2f2a4405ec69c2a3d258
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-15 21:54:29 +0200

    add html and css for current theme from views

--- workshop/components-workshop/src/app/types/theme.ts
+++ workshop/components-workshop/src/app/types/theme.ts
@@ -1,8 +1,9 @@
+import { Post } from "./post";
 import { UserId } from "./user-id";
 
 export interface Theme {
   subscribers: string[];
-  posts: string[];
+  posts: any;//string[] | Post[];
   _id: '5fa64a9f2183ce1728ff371a';
   themeName: string;
   userId: UserId;
