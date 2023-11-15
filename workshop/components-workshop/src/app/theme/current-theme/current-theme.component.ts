2ead3bdab54059d971326395c8a92b7f388f76f8
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-15 21:12:54 +0200

    implements OnInit function which call fetchTheme

--- workshop/components-workshop/src/app/theme/current-theme/current-theme.component.ts
+++ workshop/components-workshop/src/app/theme/current-theme/current-theme.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { Theme } from 'src/app/types/theme';
@@ -8,16 +8,20 @@ import { Theme } from 'src/app/types/theme';
   templateUrl: './current-theme.component.html',
   styleUrls: ['./current-theme.component.css'],
 })
-export class CurrentThemeComponent {
+export class CurrentThemeComponent implements OnInit {
   theme: Theme | undefined;
 
   constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.fetchTheme();
+  }
 
   fetchTheme():void {
     const id = this.activatedRoute.snapshot.params['themeId'];
-    
+
     this.apiService.getTheme(id).subscribe(theme => {
       this.theme = theme;
+      console.log({ theme });
     });
 
   }
