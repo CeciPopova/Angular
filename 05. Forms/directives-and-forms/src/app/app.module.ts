92d8a6c6b0ee1b6c211ac2e20b3ab5b5d9399b50
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-17 11:52:15 +0200

    generate components home, login and register and my-router-link.directive

--- 05. Forms/directives-and-forms/src/app/app.module.ts
+++ 05. Forms/directives-and-forms/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightOnMoveDirective } from './highlight-on-move.directive';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyRouterLinkDirective } from './my-router-link.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightOnMoveDirective,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyRouterLinkDirective
   ],
   imports: [
     BrowserModule,
