54477569c9a1d2e3d3ac01ddb8abb8d07107ab23
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-17 13:46:01 +0200

    Add paths to routes in app-routing module

--- 05. Forms/directives-and-forms/src/app/my-router-link.directive.ts
+++ 05. Forms/directives-and-forms/src/app/my-router-link.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Directive({
   selector: '[appMyRouterLink]'
 })
-export class MyRouterLinkDirective {
+export class MyRouterLinkDirective implements OnInit {
+  @Input() appMyRouterLink: string = '';
 
-  constructor() { }
+  constructor(private elRaf: ElementRef, private renderer: Renderer2, private router: Router ) { }
+  ngOnInit(): void {
+    this.renderer.listen(
+      this.elRaf.nativeElement,
+      'click',
+      this.clickHandler.bind(this)
+    )
+  }
 
+  clickHandler(e: MouseEvent){
+    console.log(this.appMyRouterLink);
+    this.router.navigate([this.appMyRouterLink]);
+  }
 }
