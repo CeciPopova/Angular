8748b94bb4294c0f3bb21418fbf5c46948e1d8dc
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-17 11:49:10 +0200

    Create new demo app

---
+++ 05. Forms/directives-and-forms/src/app/highlight-on-move.directive.ts
@@ -0,0 +1,62 @@
+import { Element } from '@angular/compiler';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightOnMove]',
+})
+export class HighlightOnMoveDirective implements OnInit, OnDestroy {
+  @HostListener('mouseover', ['$event']) mmouseOverHandler(e: MouseEvent) {
+    console.log(e);
+  }
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  unsubscribeFromEvent: (() => void)[] = [];
+
+  ngOnInit(): void {
+    console.log(this.elRef);
+
+    //this.elRef.nativeElement.style.backgroundColor = 'yellow';
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+
+    this.unsubscribeFromEvent.push(
+      this.renderer.listen(
+        this.elRef.nativeElement,
+        'mouseenter',
+        this.mouseEnterHandler.bind(this)
+      )
+    );
+
+    this.unsubscribeFromEvent.push(
+      this.renderer.listen(
+        this.elRef.nativeElement,
+        'mouseleave',
+        this.mouseLeaveHandler.bind(this)
+      )
+    );
+  }
+
+  mouseEnterHandler(e: MouseEvent): void {
+    //console.log('enter ',e);
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+
+    this.renderer.addClass(this.elRef.nativeElement, 'highlight');
+  }
+
+  mouseLeaveHandler(e: MouseEvent): void {
+    //console.log('leave ',e);
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'white');
+    this.renderer.removeClass(this.elRef.nativeElement, 'highlight');
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeFromEvent.forEach((fn) => fn());
+  }
+}
