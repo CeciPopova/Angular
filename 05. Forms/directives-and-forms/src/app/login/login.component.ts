f640e04f559d6ca194336edc4ca45a18d32fe07a
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-11-30 22:59:12 +0200

    validations for email and password

--- 05. Forms/directives-and-forms/src/app/login/login.component.ts
+++ 05. Forms/directives-and-forms/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,8 +6,30 @@ import { NgForm } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  submitHandler(form: NgForm): void {
-    console.log(form);
+export class LoginComponent implements OnInit{
+  //another way to get the form login without passing it as a parameter of the callback
+  @ViewChild('login') loginForm: NgForm | undefined;
+
+  ngOnInit(): void {
+    //will render only the static content
+  }
+
+  ngAfterViewInit(): void {
+    //will render the final content with dinamic components
+    this.loginForm?.valueChanges?.subscribe(console.log);
+  }
+  submitHandler(): void {
+    if (!this.loginForm) {
+      return;
+    }
+
+    const form = this.loginForm
+    if (form.invalid) {
+      return;
+    }
+
+    form.setValue({email: '', password: '' });
+    const value: { email: string; password: string } = form.value;
+    console.log(value);
   }
 }
