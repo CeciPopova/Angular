adef944d6347a32c76851b564629beeb9920b0a6
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-12-03 08:47:58 +0200

    Validation of email done

---
+++ workshop/components-workshop/src/app/shared/validators/app-email-validator.ts
@@ -0,0 +1,13 @@
+import { ValidatorFn } from "@angular/forms";
+
+
+export function appEmailValidator(domains: string[]): ValidatorFn {
+    const  domainString = domains.join('|') // ['bg','com'] => bg|com
+    const regEx = new RegExp(`[^@]{6,}@gmail\.(${domainString})$`);
+
+
+    return (control) => {
+
+        return control.value === '' || regEx.test(control.value) ? null : { appEmailValidator: true};
+    };
+}
