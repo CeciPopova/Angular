a65cb96f59f42466eea78c68332d50bff468e983
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2024-01-12 13:14:50 +0200

    validations in register- reactive forms

--- workshop/components-workshop/src/app/user/register/register.component.ts
+++ workshop/components-workshop/src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+import { DEFAULT_IMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/validators/match-password-validator';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ["", [Validators.required, Validators.minLength(5)]],
+    email: ["", [Validators.required, appEmailValidator(DEFAULT_IMAIL_DOMAINS)]],
+    tel: [""],
+    passGroup: this.fb.group({
+      password: ["", [Validators.required, Validators.minLength(5)]],
+      rePassword: ["", [Validators.required]],
+    },
+    {
+      validators: [matchPasswordsValidator("password", "rePassword")],
+    }
+    ),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  register(): void{
+    console.log(this.form.value);
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
